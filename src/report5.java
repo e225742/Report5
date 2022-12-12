080ea7da7c5471967a9aa52d46305ae25deb4ddd
Takuyuki Kawakami <e225742@ie.u-ryukyu.ac.jp>
2022-12-13 00:40:37 +0900

    3rd step

--- src/report5.java
+++ src/report5.java
@@ -1,8 +1,9 @@
 public class report5{
     public static void main(String[] args){
         try{
-            String str = null;
-            System.out.println(str.length());
+            String str = "壱百満";
+            int value = Integer.parseInt(str);
+            System.out.println(value);
         }catch(NullPointerException e){
             System.out.println("NullPointerExceptionが発生しました。" + e.getMessage());
         }
